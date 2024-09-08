48f261c0c1c4bcbf5ead0f641e9686f011ed28da
Tadeusz <szuta.tadeusz@gmail.com>
2024-09-08 21:30:30 +0200

    Added AJAX request, created function to generate country cards

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
 'use strict';
-
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+// https://countries-api-836d.onrender.com/countries/
+
+const getCountryData = function (country) {
+  // Creating new AJAX request
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  // Listening for data to arrive
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+
+    // Generating HTML to present data about country
+    const html = `<article class="country">
+          <img class="country__img" src="${data.flags.svg}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}M</p>
+            <p class="country__row"><span>🗣️</span>${
+              data.languages[Object.keys(data.languages)[0]]
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[Object.keys(data.currencies)[0]].name
+            }</p>
+          </div>
+        </article>`;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+  });
+};
+
+getCountryData('South Korea');
+getCountryData('Japan');
