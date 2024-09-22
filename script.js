ba5c3f450e90cca8d7a38a9ff38c887c5a2c4491
TeddyS <szuta.tadeusz@gmail.com>
2024-09-22 08:33:20 +0200

    Testing the event loop

--- script.js
+++ script.js
@@ -166,3 +166,14 @@ btn.addEventListener('click', function () {
 // btn.addEventListener('click', function () {
 //   getCountryData('Poland');
 // });
+
+console.log('test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Promise resolved 2').then(res => {
+  for (let i = 0; i < 100000000; i++) {}
+  console.log(res);
+});
+
+console.log('test end');
